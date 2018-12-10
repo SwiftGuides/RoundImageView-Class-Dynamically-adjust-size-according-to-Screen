fa6bcb1045b408d440618d8c2c2e5469b2cb02ff
Mr. EndLess <aryanmk.mk5@gmail.com>
2018-12-10 10:06:20 +0530

    Update myRoundImageClass.swift

--- myRoundImageClass.swift
+++ myRoundImageClass.swift
@@ -13,7 +13,7 @@ import UIKit
 class RoundImageView: UIImageView {
     
     override func layoutSubviews() {   
-        super.layoutSubviews()
+        super.layoutSubviews() //First Changes the View according to the screen and after then round image code runs written below
         self.layer.cornerRadius = self.bounds.size.width / 2
         self.clipsToBounds = true
     }
